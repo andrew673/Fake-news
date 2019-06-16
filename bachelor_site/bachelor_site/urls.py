129ff575cb7d0051a4c8467f9c0f2d338081709a
andrew673 <and96x@gmail.com>
2019-06-17 00:18:14 +0300

    Licenta functionala

--- bachelor_site/bachelor_site/urls.py
+++ bachelor_site/bachelor_site/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
--- bachelor_site/polls/views.py
+++ bachelor_site/polls/views.py
@@ -6,6 +6,83 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+import os
+import time
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+	if request.method == 'GET':
+		print("hello")
+	else:
+		filename = request.POST.get("filename", '')
+		single = request.POST.get("single", '')
+		collection = request.POST.get("collection", '')
+		art_no = request.POST.get("art_no", '')
+		nmf = request.POST.get("nmf", '')
+		lda = request.POST.get("lda", '')
+		lsa = request.POST.get("lsa", '')
+		enhanced_lda = request.POST.get("enhanced_lda", '')
+		labels_no = request.POST.get("labels_no", '')
+		labels = request.POST.get("labels", '')
+		visualize = request.POST.get("visualize", '')
+
+		if single.startswith( 'on' ):
+			single = '--unique'
+		else:
+			single = ''
+		if collection.startswith( 'on' ):
+			collection = '--collection'
+		else:
+			collection = ''
+		if nmf.startswith( 'on' ):
+			nmf = '--nmf'
+		else:
+			nmf = ''
+		if enhanced_lda.startswith( 'on' ):
+			enhanced_lda = '--enhanced_lda'
+		else:
+			nmf = ''
+		if lda.startswith( 'on' ):
+			lda = '--lda'
+		else:
+			lda = ''
+		if lsa.startswith( 'on' ):
+			lsa = '--lsa'
+		else:
+			lsa = ''
+		if visualize.startswith( 'on' ):
+			visualize = '--visualize'
+		else:
+			visualize = ''
+		command = "python3 /home/andrew/Desktop/Licenta/Fake-news/bachelor_site/code/bachelor_project.py"
+		command += " "
+		command += filename
+		command += " "
+		command += single
+		command += " "
+		command += collection
+		command += " "
+		command += art_no
+		command += " "
+		command += nmf
+		command += " "
+		command += lda
+		command += " "
+		command += enhanced_lda
+		command += " "
+		command += lsa
+		command += " "
+		command += "--labels "
+		command += labels_no
+		command += " "
+		command += labels
+		command += " > /home/andrew/Desktop/Licenta/Fake-news/bachelor_site/code/output.txt"
+		path = "/home/andrew/Desktop/Licenta/Fake-news/bachelor_site/code/output.txt"
+		os.system(command)
+		file = open(path,'r')
+		response_text = file.read()
+		return HttpResponse(response_text)
+
+	return render(request, 'home.html')
+	#return HttpResponse("Hello, world. You're at the polls index.")
